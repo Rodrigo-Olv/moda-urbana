4ad211cb9a0cb0e3b5656a22086f75a06d62b34c
Rodrigo-Olv <rodrigo.pjs.olv@gmail.com>
2024-12-08 12:32:58 +0100

    actualización db

--- moda_urbana_web/db.php
+++ moda_urbana_web/db.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'localhost'; 
-$db_name = 'moda_urbana_db'; 
-$username = 'root'; 
-$password = ''; 
+$host = 'sql201.infinityfree.com'; 
+$db_name = 'if0_37726260_moda_urbana_db'; 
+$username = 'if0_37726260'; 
+$password = 'FtezpWVnDq'; 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
